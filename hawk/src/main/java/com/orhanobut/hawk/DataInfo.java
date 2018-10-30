c6b8d3a82cbabdf0c4c9fe5846f11b1a2982bd17
Ruben Gees <rubengees7@googlemail.com>
2018-10-30 20:37:27 +0100

    Make fields of DataInfo public to use in converters

--- hawk/src/main/java/com/orhanobut/hawk/DataInfo.java
+++ hawk/src/main/java/com/orhanobut/hawk/DataInfo.java
@@ -2,15 +2,15 @@ package com.orhanobut.hawk;
 
 public final class DataInfo {
 
-  static final char TYPE_OBJECT = '0';
-  static final char TYPE_LIST = '1';
-  static final char TYPE_MAP = '2';
-  static final char TYPE_SET = '3';
+  public static final char TYPE_OBJECT = '0';
+  public static final char TYPE_LIST = '1';
+  public static final char TYPE_MAP = '2';
+  public static final char TYPE_SET = '3';
 
-  final char dataType;
-  final String cipherText;
-  final Class keyClazz;
-  final Class valueClazz;
+  public final char dataType;
+  public final String cipherText;
+  public final Class keyClazz;
+  public final Class valueClazz;
 
   DataInfo(char dataType, String cipherText, Class keyClazz, Class valueClazz) {
     this.cipherText = cipherText;
